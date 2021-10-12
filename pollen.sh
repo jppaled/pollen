737c93e9cd13c35fbd1cae5756074440b9bdd14e
jppaled <22444128+jppaled@users.noreply.github.com>
2021-10-12 11:04:37 +0200

    add coms to the code

--- pollen.sh
+++ pollen.sh
@@ -13,20 +13,29 @@ get_translated_month () {
     printf ${enMonths[$i]}
 }
 
+# html bloc where the date is
 html=$(curl -s https://pollens.fr/ | grep "Carte de vigilance");
 
+# remove html tag
 title=$(echo $html | sed 's/<[^>]*>//g ; /^$/d');
 
+# remove useless text to get the french date
 date=$(echo $title | sed s/"Carte de vigilance - mise &agrave; jour le "// | sed 's/\r$//');
 
+# convert to array
 dateArr=( $date );
 
+# get the day number
 day="${dateArr[0]}";
 
+# get the french month
 frMonth="${dateArr[1]}";
 
+# convert to english month
 enMonth=$(get_translated_month $frMonth);
 
+# create date
 finalDate=$(date -d "$day"-"$enMonth" +%Y-%m-%d);
 
+# retrieve the image and save it with the date as name
 curl -o "$path_to_save_img/pollen-$finalDate.png" https://pollens.fr/generated/vigilance_map.png
